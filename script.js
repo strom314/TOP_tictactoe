b869bfa434002b00d727a47742b2b02e9ee456f9
strom314 <jakub.gros777@gmail.com>
2025-09-21 21:09:26 +0200

    add gamemanager factory and winPatterns array

--- script.js
+++ script.js
@@ -9,16 +9,28 @@ const gameBoard = (function () {
   function printBoard() {
     console.log(board);
   }
-  function getBoard(){
+  function getBoard() {
     return board;
   }
 
   return { placeSymbol, printBoard };
 })();
 
-const gameManager = (function (){
+const gameManager = (function () {
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  function checkForWin(symbol){
     
-})()
+  }
+})();
 
 function createPlayer(name, symbol) {
   function play(index) {
